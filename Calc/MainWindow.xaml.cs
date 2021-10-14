08b06f3bcf5ddf58a4b3434c865ffb50e87de6e8
SonnyFixit <fikser@tlen.pl>
2021-10-14 23:39:28 +0200

    Basic button functionality

--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private double lastNumber;
+        private double result;
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            numberSeven.Click += NumberSeven_Click;
+            acButton.Click += AcButton_Click;
+            positiveNegativeButton.Click += PositiveNegativeButton_Click;
+            equalsButton.Click += EqualsButton_Click;
+            percentButton.Click += PercentButton_Click;
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber /100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NumberSeven_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+
+            else
+            {
+
+            }
+        }
+
+        private void PositiveNegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
         }
     }
 }
